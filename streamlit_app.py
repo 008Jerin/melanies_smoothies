163e99331ea8fe02da82693aa7a4c6541e46af57
Jerin <93442466+008Jerin@users.noreply.github.com>
2026-06-28 16:36:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit')
 
-  import requests  
+import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 st.text(smoothiefroot_response)
 
