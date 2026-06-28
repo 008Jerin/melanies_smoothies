5dd1be6ed0343d649b149454dbcc65a745644ba0
Jerin <93442466+008Jerin@users.noreply.github.com>
2026-06-28 16:34:02 +0530

    Update streamlit_app.py
    importing requests for REST API calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,10 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit')
 
+  import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
+
     if time_to_insert   :
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered! '+name_on_order, icon="✅")
