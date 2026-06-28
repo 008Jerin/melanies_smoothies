88ccc50e736383a35232c31ca0c6f55b120cf20d
Jerin <93442466+008Jerin@users.noreply.github.com>
2026-06-28 16:46:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
     time_to_insert = st.button('Submit')
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon]")  
 st.text(smoothiefroot_response.json())
 
 if time_to_insert:
